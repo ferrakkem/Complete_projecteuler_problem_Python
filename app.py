2d31f209d0727f80316a3545aeba1479666ab014
ferrakkem <ferrakkem@gmail.com>
2020-04-20 22:04:16 -0400

    Second commit of multiples_of_three_and_five() projecteuler problem in Python

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 '''
 
 def multiples_of_three_and_five(numbers):
-    print(numbers)
+    sum = 0
+    for num in numbers:
+        if (num%3) == 0 or (num%5)==0:
+            print(num)
+            sum = sum + num
+    return sum
 
-
-print(multiples_of_three_and_five([10, 8, 16, 16, 19, 20]))
+print(multiples_of_three_and_five([3, 5, 6, 9]))
