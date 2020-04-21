d4d6ef32b32570714e07db2f8d4c733d7de76ba6
ferrakkem <ferrakkem@gmail.com>
2020-04-20 22:32:21 -0400

    adding comment

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 def multiples_of_three_and_five(numbers):
     sum = 0
     for num in numbers:
+        #check number in between 10 to 1000
         if num >=10 and num <= 1000:
+            #checking numbers are  multiples of 3 or 5
             if (num % 3) == 0 or (num % 5) == 0:
                 sum = sum + num
     return sum
