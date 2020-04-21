0c21aa82a3a3bcc5a0936786a7af5a6b56cf45ad
ferrakkem <ferrakkem@gmail.com>
2020-04-20 21:29:31 -0400

    Initail commit of projecteuler problem in Python

---
+++ app.py
@@ -0,0 +1,13 @@
+'''
+Problem 1
+Multiples of 3 and 5
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+'''
+
+def multiples_of_three_and_five(numbers):
+    print(numbers)
+
+
+print(multiples_of_three_and_five([10, 8, 16, 16, 19, 20]))
